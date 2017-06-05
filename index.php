92e047a1a8e357efb0af1a4a25667aee41c41b93
Catalina Chung <chung.kkat@gmail.com>
2017-06-05 14:19:11 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,184 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Street Art y Diseño Gráfico en Chile">
+    <meta name="author" content="Catalina Chung">
+
+    <title>Street Art Chileno</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Theme CSS -->
+    <link href="css/clean-blog.min.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href="https://fonts.googleapis.com/css?family=Permanent+Marker" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-default navbar-custom navbar-fixed-top">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    Menu <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand" href="index.php">Street Art Chileno</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a href="index.php">Artículos</a>
+                    </li>
+                    <li>
+                        <a href="about.php">Bibliografía</a>
+                    </li>
+                    <li>
+                        <a href="post.php">Visualizaciones</a>
+                    </li>
+                    <li>
+                        <a href="contact.php">Antecedentes</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Page Header -->
+    <!-- Set your background image for this header on the line below. -->
+    <header class="intro-header" style="background-image: url('img/INTI_1.jpg')">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                    <div class="site-heading">
+                        <h1>Street Art en Chile</h1>
+                        <hr class="small">
+                        <span class="subheading">Una vista del Street Art Chileno desde el Diseño Gráfico</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <!-- Main Content -->
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+              <h2>Sobre el proyecto</h2>
+              <p>Dentro de los últimos 10 años, en Santiago han aparecido una serie de artistas callejeros que se dedican al muralismo y el street art. Destacados en sus áreas, estos artistas han sido capaces de llevar su arte por todo el mundo, llevar consigo nuestro imaginario chileno y plasmarlo en los diferentes países. No sorprende su manejo del color y la composición a gran escala, ya que los artistas que se tomarán en la investigación son también diseñadores gráficos. A partir de una serie de entrevistas a los artistas Inti, Payo y Estoy, develaremos cuál es el rol o la influencia del Diseño dentro de sus trabajos y su visión del arte callejero en Chile.</p>
+              <hr>
+              <h2>Artículos relacionados</h2>
+              <?php
+              // basta con la línea de PHP para llamar al imdb-movies.csv y asignarlo a la variable $csv
+              $csv = array_map('str_getcsv', file('datos1.csv'));
+              // pero debo hacer un pequeño ajuste, para eliminar del arreglo el encabezado del imdb-movies.csv
+              array_walk($csv, function(&$a) use ($csv) {$a = array_combine($csv[0], $a);});
+              array_shift($csv);
+              // ahora puedo crear un bucle "for(){}" que examine lo asignado como contenido a la variable $csv
+              // lo que esté contenido en la variable se repetirá tantas veces como arreglos tenga la variable $csv
+              for($a = 0; $a < $total = count($csv); $a++){?>
+
+                <article class="row">
+                  <hr>
+                  <div class="col-xs-5 col-sm-3 col-md-2"><img src="<?php echo $csv[$a]['Imagen'];?>" class="img-responsive"></div>
+                  <div class="col-xs-7 col-sm-9 col-md-10">
+
+                    <div class="post-preview">
+                        <a href="single.php?url=<?php print($a)?>">
+                    <h3><?php echo $csv[$a]['titulo'];?> </h3></a>
+                    <p><?php echo $csv[$a]['abstract'];?> </p>
+                    <h6><?php echo $csv[$a]['apa'];?> </h6>
+                    <img src="<?php echo $csv[$a]['images'];?>" class="img-responsive">
+                    <h5><a href="#"><?php echo $csv[$a]['url'];?></a></h5>
+
+                    <!--<h6>Agreguen aquí la información en Data</h6>-->
+
+                  </div>
+                </article>
+              <?php };?>
+                </div>
+                <hr>
+
+            </div>
+        </div>
+    </div>
+
+    <hr>
+
+    <!-- Footer -->
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                    <ul class="list-inline text-center">
+                        <li>
+                            <a href="https://twitter.com/?lang=es">
+                                <span class="fa-stack fa-lg">
+                                    <i class="fa fa-circle fa-stack-2x"></i>
+                                    <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
+                                </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.facebook.com/">
+                                <span class="fa-stack fa-lg">
+                                    <i class="fa fa-circle fa-stack-2x"></i>
+                                    <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
+                                </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://github.com/ChungKat">
+                                <span class="fa-stack fa-lg">
+                                    <i class="fa fa-circle fa-stack-2x"></i>
+                                    <i class="fa fa-github fa-stack-1x fa-inverse"></i>
+                                </span>
+                            </a>
+                        </li>
+                    </ul>
+                    <p class="copyright text-muted">Copyright &copy; Catalina Chung 2017</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- jQuery -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Contact Form JavaScript -->
+    <script src="js/jqBootstrapValidation.js"></script>
+    <script src="js/contact_me.js"></script>
+
+    <!-- Theme JavaScript -->
+    <script src="js/clean-blog.min.js"></script>
+
+</body>
+
+</html>
---
+++ single.php
@@ -0,0 +1,187 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Street Art y Diseño Gráfico en Chile">
+    <meta name="author" content="Catalina Chung">
+
+    <title>Street Art Chileno</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Theme CSS -->
+    <link href="css/clean-blog.min.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href="https://fonts.googleapis.com/css?family=Permanent+Marker" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-default navbar-custom navbar-fixed-top">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    Menu <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand" href="index.php">Street Art Chileno</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a href="index.php">Artículos</a>
+                    </li>
+                    <li>
+                        <a href="about.php">Bibliografía</a>
+                    </li>
+                    <li>
+                        <a href="post.php">Visualizaciones</a>
+                    </li>
+                    <li>
+                        <a href="contact.php">Antecedentes</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Page Header -->
+    <!-- Set your background image for this header on the line below. -->
+    <header class="intro-header" style="background-image: url('img/INTI_1.jpg')">
+      <?php
+      $csv = array_map('str_getcsv', file('datos2.csv'));
+      array_walk($csv, function(&$a) use ($csv) {$a = array_combine($csv[0], $a);});
+      array_shift($csv);
+      $la_url = $_GET['url'];
+      ?>
+
+      <div class="container">
+          <div class="row">
+              <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                  <div class="site-heading">
+                      <h1><?php echo($csv[$la_url]["titulo"])?></h1>
+                      <hr class="small">
+                      <span class="subheading"><?php echo($csv[$la_url]["url"]);?></span>
+                  </div>
+              </div>
+          </div>
+      </div>
+      </header>
+
+    <!-- Main Content -->
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+              <?php
+              $csv = array_map('str_getcsv', file('datos2.csv'));
+              array_walk($csv, function(&$a) use ($csv) {$a = array_combine($csv[0], $a);});
+              array_shift($csv);
+              $la_url = $_GET['url'];
+              ?>
+
+              <div class="container">
+                  <div class="row">
+                      <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                          <div class="site-heading">
+                              <span class="subheading"><?php echo($csv[$la_url]["abstract"]);?></span>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              </header>
+
+
+                  <!-- Post Content -->
+                  <article>
+                      <div class="container">
+                          <div class="row">
+                              <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+
+                                <?php echo($csv[$la_url]["texto"])?>
+
+                            </div>
+                          </div>
+                      </div>
+                  </article>
+
+                  <hr>
+
+                </div>
+
+
+    <!-- Footer -->
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                    <ul class="list-inline text-center">
+                        <li>
+                            <a href="https://twitter.com/?lang=es">
+                                <span class="fa-stack fa-lg">
+                                    <i class="fa fa-circle fa-stack-2x"></i>
+                                    <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
+                                </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.facebook.com/">
+                                <span class="fa-stack fa-lg">
+                                    <i class="fa fa-circle fa-stack-2x"></i>
+                                    <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
+                                </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://github.com/ChungKat">
+                                <span class="fa-stack fa-lg">
+                                    <i class="fa fa-circle fa-stack-2x"></i>
+                                    <i class="fa fa-github fa-stack-1x fa-inverse"></i>
+                                </span>
+                            </a>
+                        </li>
+                    </ul>
+                    <p class="copyright text-muted">Copyright &copy; Catalina Chung 2017</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- jQuery -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Contact Form JavaScript -->
+    <script src="js/jqBootstrapValidation.js"></script>
+    <script src="js/contact_me.js"></script>
+
+    <!-- Theme JavaScript -->
+    <script src="js/clean-blog.min.js"></script>
+
+</body>
+
+</html>
